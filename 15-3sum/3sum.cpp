6534ba9fed840498514f9ff92b0dc5cbfdfe0e30
Sachin Kumar <111720618+sachinkumarS-K@users.noreply.github.com>
2025-02-13 10:18:21 +0530

    Time: 93 ms (23.77%) | Memory: 117.9 MB (16.27%) - LeetSync

---
+++ 15-3sum/3sum.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<vector<int>> twoSum(vector<int> arr, int n, int t) {
+
+        vector<vector<int>> ans;
+        int l = n, h = arr.size() - 1;
+        while (l < h) {
+            int s = arr[l] + arr[h];
+
+            if (s == t) {
+                ans.push_back({arr[n - 1], arr[l], arr[h]});
+                while (l < h && arr[l] == arr[l + 1])
+                        l++;
+                while (l < h && arr[h] == arr[h - 1])
+                        h--;
+                l++;
+                h--;
+            } else if (s > t) {
+                h--;
+            } else {
+                l++;
+            }
+        }
+        return ans;
+    }
+
+    vector<vector<int>> threeSum(vector<int> arr) {
+
+        vector<vector<int>> ans;
+        sort(arr.begin(), arr.end());
+
+        for (int i = 0; i < arr.size(); i++) {
+            if (i > 0 && arr[i] == arr[i - 1])
+                continue;
+            vector<vector<int>> pairs = twoSum(arr, i + 1, -arr[i]);
+            ans.insert(ans.end(), pairs.begin(), pairs.end());
+        }
+        return ans;
+    }
+};
