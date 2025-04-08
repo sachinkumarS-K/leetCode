b55c3bdf9510e27587c040299ff577ba8cca9620
Sachin Kumar <111720618+sachinkumarS-K@users.noreply.github.com>
2025-04-08 19:25:55 +0530

    Time: 3 ms (42.32%) | Memory: 10 MB (88.18%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,12 +1,13 @@
 class Solution {
-    void reverse(string & str , int l , int h){
-        while(l < h)
-            swap(str[l++] , str[h--]);        
+    void reverse(string& str, int l, int h) {
+        while (l < h)
+            swap(str[l++], str[h--]);
     }
+
 public:
     string reverseWords(string s) {
         string ans;
-        int i = 0 , n = s.length();
+        int i = 0, n = s.length();
         while (i < n && s[i] == ' ')
             i++;
         bool spaceAdded = false;
@@ -18,23 +19,21 @@ public:
                 ans += ' ';
                 spaceAdded = true;
             }
-        }    
-    
-        if(!ans.empty() && ans.back() == ' ') 
+        }
+
+        if (!ans.empty() && ans.back() == ' ')
             ans.pop_back();
 
-        reverse(ans , 0 , ans.size() - 1);
+        reverse(ans, 0, ans.size() - 1);
 
-        int idx = 0 ;
-        for(i = 0 ; i<ans.length() ; i++){
-            if(ans[i] == ' '){
-                reverse(ans , idx , i-1);
-                idx = i+1;
+        int idx = 0;
+        for (i = 0; i < ans.length(); i++) {
+            if (ans[i] == ' ') {
+                reverse(ans, idx, i - 1);
+                idx = i + 1;
             }
         }
-        reverse(ans , idx , ans.size()-1);
-
-
+        reverse(ans, idx, ans.size() - 1);
 
         return ans;
     }
