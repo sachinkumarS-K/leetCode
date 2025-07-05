47498bc2823d863b21fe0ec197e49e9d3fc6a29e
Sachin Kumar <111720618+sachinkumarS-K@users.noreply.github.com>
2025-07-05 21:25:43 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (8.79%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,29 +1,55 @@
+class Node {
+public:
+    char data;
+    Node* child[26];
+    bool isTerminal;
+    Node(char d) {
+        data = d;
+        isTerminal = false;
+        for (int i = 0; i < 26; i++)
+            child[i] = nullptr;
+    }
+};
+
+void insert(Node* root, string word) {
+    for (char& ch : word) {
+        int i = ch - 'a';
+        if (root->child[i] == nullptr)
+            root->child[i] = new Node(ch);
+
+        root = root->child[i];
+    }
+    root->isTerminal = true;
+}
+
+void findPrefix(Node* root, string& str) {
+    int c = 0;
+    int next;
+    for (int i = 0; i < 26; i++) {
+        if (root->child[i] != nullptr) {
+            c++;
+            next = i;
+        }
+    }
+    if (c > 1 || root->isTerminal) {
+        return ;
+    }
+    str += (char)(next + 'a');
+
+    findPrefix(root->child[next], str);
+}
+
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        int i = 0;
-        string ans  ;
-        while(1){
-            char curr = 0;
-            for(auto str : strs){
-                if(i >= str.size()){
-                    curr = 0;
-                    break;
-                }
-                if(curr == 0){
-                    curr = str[i];
-                }
-                else if(str[i] != curr){
-                    curr = 0;
-                    break;
-                }        
-            }
-            if(curr == 0)
-                break;
-            
-            ans.push_back(curr);
-            i++;
-        }
+        Node* root = new Node('\0');
+        if (strs.empty())
+            return "";
+
+         for (string& word : strs) insert(root, word);
+
+        string ans = "";
+        findPrefix(root, ans);
         return ans;
     }
 };
