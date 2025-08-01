5e131bd5029590bed1d4a270bb20c8dfdc5d7064
Sachin Kumar <111720618+sachinkumarS-K@users.noreply.github.com>
2025-08-01 21:57:21 +0530

    Time: 51 ms (51.46%) | Memory: 31 MB (28.47%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,39 +1,31 @@
 class Solution {
 public:
-    vector<vector<int>> twoSum(vector<int> arr, int n, int t) {
-        vector<vector<int>> ans;
-        int l = n, h = arr.size() - 1;
-        while (l < h) {
-            int s = arr[l] + arr[h];
+    vector<vector<int>> threeSum(vector<int>& arr) {
+        vector<vector<int>>ans;
+        sort(arr.begin() , arr.end());
+        int n = arr.size();
+        for(int i = 0 ; i<n ; i++){
+            if(i > 0 && arr[i] == arr[i-1]) continue;
 
-            if (s == t) {
-                ans.push_back({arr[n - 1], arr[l], arr[h]});
-                while (l < h && arr[l] == arr[l + 1])
-                    l++;
-                while (l < h && arr[h] == arr[h - 1])
-                    h--;
-                l++;
-                h--;
-            } else if (s > t) {
-                h--;
-            } else {
-                l++;
+            int j = i + 1;
+            int k = n-1 ;
+            while(j < k){
+                int sum = arr[i]+arr[j]+arr[k] ;
+                if(sum < 0){
+                    j++;
+                }
+                else if(sum > 0){
+                    k-- ;
+                }
+                else{
+                    vector<int>temp = {arr[i] , arr[j] , arr[k]};
+                    ans.push_back(temp);
+                    j++ , k-- ;
+                    while(j < k && arr[j] == arr[j-1]) j++;
+                    while(j < k && arr[k] == arr[k+1]) k-- ;
+                }
             }
-        }
-        return ans;
-    }
-
-    vector<vector<int>> threeSum(vector<int> arr) {
-
-        vector<vector<int>> ans;
-        sort(arr.begin(), arr.end());
-
-        for (int i = 0; i < arr.size(); i++) {
-            if (i > 0 && arr[i] == arr[i - 1])
-                continue;
-            vector<vector<int>> pairs = twoSum(arr, i + 1, -arr[i]);
-            ans.insert(ans.end(), pairs.begin(), pairs.end());
-        }
+        }      
         return ans;
     }
 };
