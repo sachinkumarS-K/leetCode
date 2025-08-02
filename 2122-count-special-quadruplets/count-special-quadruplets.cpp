6e968d76e1c0a91ef42d762aaf206fc16a66bd99
Sachin Kumar <111720618+sachinkumarS-K@users.noreply.github.com>
2025-08-02 20:40:14 +0530

    Time: 14 ms (36.02%) | Memory: 13.4 MB (97.20%) - LeetSync

---
+++ 2122-count-special-quadruplets/count-special-quadruplets.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countQuadruplets(vector<int>& arr) {
+        int count = 0, n = arr.size();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    for (int l = k + 1; l < n; l++) {
+                        if (arr[i] + arr[j] + arr[k] == arr[l]) {
+                            count++;
+                        }
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+};
