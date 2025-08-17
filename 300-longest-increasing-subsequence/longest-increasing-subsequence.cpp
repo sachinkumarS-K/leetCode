caaf595a46133facbe91dee9eefa07520e47ffca
Sachin Kumar <111720618+sachinkumarS-K@users.noreply.github.com>
2025-08-17 14:48:42 +0530

    Time: 297 ms (32.53%) | Memory: 296.4 MB (31.06%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,19 +1,34 @@
 class Solution {
 public:
-    int solve(vector<int>&arr , int curr , int prev ,  vector<vector<int>>&dp){
+    int solveUsingemo(vector<int>&arr , int curr , int prev ,  vector<vector<int>>&dp){
         if(curr >= arr.size())
             return 0 ;
         if(dp[curr][prev+1] != -1)  return dp[curr][prev+1];
         int include =  0 ;
         if(prev == -1 || arr[curr] > arr[prev])
-            include = 1 + solve(arr , curr+1 , curr , dp);
-        int exclude = solve(arr , curr + 1 , prev , dp) ;
+            include = 1 + solveUsingemo(arr , curr+1 , curr , dp);
+        int exclude = solveUsingemo(arr , curr + 1 , prev , dp) ;
 
         return dp[curr][prev+1] = max(include , exclude);
     }
+    int solveUsingTabu(vector<int>&arr){
+        int n = arr.size() ;
+        vector<vector<int>>dp(n+1,vector<int>(n+1 , 0));
+
+        for(int curr = n-1  ; curr >= 0 ; curr--){
+            for(int prev = curr-1 ; prev >= -1 ; prev--){
+                int include = 0 ;
+                if(prev == -1 || arr[curr] > arr[prev]){
+                    include = 1 + dp[curr+1][curr+1] ;
+                }
+                int exclude = dp[curr+1][prev+1];
+                dp[curr][prev+1] = max(include , exclude);
+            }
+        }
+        return dp[0][0];
+    }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size() ;
-        vector<vector<int>>dp(n,vector<int>(n+1 , -1));
-        return solve(nums , 0 , -1 , dp);
+        return solveUsingTabu(nums);
     }
 };
