6e85b54120c0e6bd668558cb3f5d3ae311ab80b7
Sachin Kumar <111720618+sachinkumarS-K@users.noreply.github.com>
2025-08-24 18:08:26 +0530

    Time: 30 ms (12.43%) | Memory: 9.4 MB (88.72%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
@@ -13,9 +13,28 @@ public:
 
         return dp[n][target] =  ans;
     }
+    int solveUsingTabulation(int n , int k , int target){
+        // vector<vector<int>>dp(n+1 , vector<int>(target+1 , 0));
+        vector<int>prev(target+1 , 0);
+        vector<int>curr(target+1 , 0);
+        prev[0] = 1 ;
+
+        for (int i = 1; i <= n; i++) {
+            for (int t = 1; t <= target; t++) {
+                long long ans = 0;
+                for (int val = 1; val <= k; val++) 
+                    if (t - val >= 0) 
+                        ans = (ans % mod + prev[t-val]) % mod;                
+                curr[t] = ans;
+            }
+            prev = curr ;
+        }
+
+        return curr[target];
+    }
     int numRollsToTarget(int n, int k, int target) {
-        vector<vector<int>>dp(n+1 , vector<int>(target+1 , -1));
-        int ans = solve(n , k , target , dp);
+        // vector<vector<int>>dp(n+1 , vector<int>(target+1 , -1));
+        int ans = solveUsingTabulation(n , k , target);
         return ans;
     }
 };
