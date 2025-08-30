808afc49a0dac65d94bb57b8530aa50181c2ff4e
Sachin Kumar <111720618+sachinkumarS-K@users.noreply.github.com>
2025-08-30 11:55:17 +0530

    Time: 9 ms (8.12%) | Memory: 19.9 MB (23.82%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -17,11 +17,27 @@ public:
 
         return dp[idx][buy] = profit;
     }
-    // int solveUsingTabulation(vector<int>&arr){
-
-    // }
+int solveUsingTabulation(vector<int>& arr) {
+        vector<vector<int>> dp(arr.size() + 1, vector<int>(2, 0));
+        for (int idx = arr.size() - 1; idx >= 0; idx--) {
+            for (int b = 0; b < 2; b++) {
+                int profit = 0 ;
+                if (b) {
+                    int include = -arr[idx] + dp[idx + 1][0];
+                    int exclude = dp[idx + 1][1];
+                    profit = max(include, exclude);
+                } else {
+                    int include = arr[idx] + dp[idx + 1][1];
+                    int exclude = dp[idx + 1][0];
+                    profit = max(include, exclude);
+                }
+                dp[idx][b] = profit;
+            }
+        }
+        return dp[0][1];
+    }
     int maxProfit(vector<int>& prices) {
-        vector<vector<int>>dp(prices.size()+1 , vector<int>(2 , -1));
-        return solve(0 , 1 , prices , dp);
+        //vector<vector<int>>dp(prices.size()+1 , vector<int>(2 , -1));
+        return solveUsingTabulation(prices);
     }
 };
