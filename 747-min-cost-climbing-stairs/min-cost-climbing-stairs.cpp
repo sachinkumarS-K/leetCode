5c596af1af57f1766c0f36a7828bd55d04935cb1
Sachin Kumar <111720618+sachinkumarS-K@users.noreply.github.com>
2025-09-11 12:40:02 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (31.03%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
-   int solve(vector<int>&cost , int idx , vector<int>&dp){
-        if(idx >= cost.size())
-            return 0 ;
-        if(dp[idx] != -1)   return dp[idx];
-        int oneStep = cost[idx] + solve(cost , idx+1 , dp);
-        int twoStep = cost[idx] + solve(cost , idx+2 , dp);
-        return dp[idx] =  min(oneStep , twoStep);
-   }
+    int solve(vector<int>& cost, int idx, vector<int>& dp) {
+        if (idx >= cost.size())
+            return 0;
+        if (dp[idx] != -1)
+            return dp[idx];
+        int oneStep = cost[idx] + solve(cost, idx + 1, dp);
+        int twoStep = cost[idx] + solve(cost, idx + 2, dp);
+        return dp[idx] = min(oneStep, twoStep);
+    }
+    int solveUsingTabulation(vector<int>& cost, int idx) {
+        int n = cost.size();
+        vector<int> dp(n + 1, 0);
+        dp[n] = 0;
+        dp[n - 1] = cost[n - 1];
+        for (int i = n - 2; i >= idx; i--) {
+            int oneStep = cost[i] + dp[i + 1];
+            int twoStep = cost[i] + dp[i + 2];
+            dp[i] = min(oneStep, twoStep);
+        }
+        return dp[idx];
+    }
 
     int minCostClimbingStairs(vector<int>& cost) {
-        int n = cost.size();
-        vector<int> dp(n, -1);
-        return min(solve(cost, 0 , dp), solve(cost, 1 , dp));
+        return min(solveUsingTabulation(cost, 0),
+                   solveUsingTabulation(cost, 1));
     }
 };
