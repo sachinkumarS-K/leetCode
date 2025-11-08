9e3884a4d76ee80e06fb92c4fa77448b52c1aa55
Sachin Kumar <111720618+sachinkumarS-K@users.noreply.github.com>
2025-11-08 15:20:50 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (77.66%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,24 +1,41 @@
 class Solution {
 public:
-    bool solve(int idx, string& s, unordered_set<string>& dict,
-               vector<int>& dp) {
-        if (idx == s.size())
+    bool solve(int i, string s, unordered_set<string>& st, vector<int>& dp) {
+        if (i == s.size())
             return true;
-        if (dp[idx] != -1)
-            return dp[idx];
+        if (dp[i] != -1)
+            return dp[i];
         string str = "";
-        for (int i = idx; i < s.size(); i++) {
-            str += s[i];
-            if (dict.find(str) != dict.end()) {
-                if (solve(i + 1, s, dict, dp))
-                    return dp[idx] = true;
+        for (int j = i; j < s.size(); j++) {
+            str += s[j];
+            if (st.find(str) != st.end()) {
+                if (solve(j + 1, s, st, dp))
+                    return dp[i] = true;
             }
         }
-        return dp[idx] = false;
+        return dp[i] = false;
+    }
+    bool solveUsingTabulation(string s, vector<string>& wordDict) {
+        unordered_set<string> st(wordDict.begin(), wordDict.end());
+        int n = s.size();
+        vector<bool> dp(n + 1, false);
+        dp[n] = true;
+        for(int i = n-1 ; i>= 0 ; i--){
+            string str = "";
+            for(int j = i ; j< n ; j++){
+                str += s[j];
+                if(st.find(str) != st.end() && dp[j+1]){
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[0];
     }
     bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_set<string> dict(wordDict.begin(), wordDict.end());
-        vector<int> dp(s.size(), -1);
-        return solve(0, s, dict, dp);
+        // unordered_set<string> st(wordDict.begin(), wordDict.end());
+        // vector<int> dp(s.size(), -1);
+        // return solve(0, s, st, dp);
+        return solveUsingTabulation(s , wordDict);
     }
 };
