457a775f649252da5e13af8bcd976da64bc4b8db
Sachin Kumar <111720618+sachinkumarS-K@users.noreply.github.com>
2025-11-12 17:48:46 +0530

    Time: 28 ms (41.37%) | Memory: 27.4 MB (51.93%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -14,11 +14,26 @@ public:
                       solve(t1, t2, n1, n2 - 1, dp));
         return dp[n1][n2] = ans;
     }
-    
+    int solveUsingTabulation(string &text1 , string &text2){
+        int n1 = text1.size();
+        int n2 = text2.size();
+
+        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, 0));
+        for(int i = 1 ; i<=n1 ; i++){
+            int ans = 0;
+            for(int j = 1 ; j<=n2 ; j++){
+                if(text1[i-1] == text2[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = max(dp[i-1][j] , dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n1][n2];
+    }
     int longestCommonSubsequence(string text1, string text2) {
 
-        int n1 = text1.size(), n2 = text2.size();
-        vector<vector<int>> dp(n1, vector<int>(n2, -1));
-        return solve(text1, text2, n1 - 1, n2 - 1, dp);
+        return solveUsingTabulation(text1 , text2);
     }
 };
