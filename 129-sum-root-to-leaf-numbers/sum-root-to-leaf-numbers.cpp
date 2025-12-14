b72deb221a6b35c5af84e403d7732b48573eb34b
Sachin Kumar <111720618+sachinkumarS-K@users.noreply.github.com>
2025-12-14 20:28:10 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (17.08%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
@@ -1,27 +1,28 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
- * };
- */
 class Solution {
 public:
-    int dfs(TreeNode* root, int sum) {
+    int sumNumbers(TreeNode* root) {
         if (!root)
             return 0;
 
-        sum = sum * 10 + root->val;
+        int ans = 0;
+        queue<pair<TreeNode*, int>> q;
 
-        if (!root->left && !root->right)
-            return sum;
+        q.push({root, root->val});
 
-        return dfs(root->left, sum) + dfs(root->right, sum);
+        while (!q.empty()) {
+            auto curr = q.front();
+            q.pop();
+
+            TreeNode* node = curr.first;
+            int sum = curr.second;
+            if (!node->left && !node->right)
+                ans += sum;
+            if (node->left) 
+                q.push({node->left, sum * 10 + node->left->val});
+            if (node->right)
+                q.push({node->right, sum * 10 + node->right->val});
+        }
+
+        return ans;
     }
-    int sumNumbers(TreeNode* root) { return dfs(root, 0); }
 };
