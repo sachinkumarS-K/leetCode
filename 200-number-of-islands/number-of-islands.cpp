25ae7de9b899d2b3b708350a98109b3806b29ec9
Sachin Kumar <111720618+sachinkumarS-K@users.noreply.github.com>
2025-12-15 17:58:45 +0530

    Time: 23 ms (83.70%) | Memory: 16.7 MB (62.52%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,43 +1,41 @@
 class Solution {
 public:
-    bool check(int i , int j , int r, int c ,vector<vector<char>>& grid ,vector<vector<bool>>&visited){
-        if((i < r && i >= 0) && (j < c && j >= 0) && !visited[i][j] && grid[i][j] == '1')
-            return true;
-        else
-            return false;
+    bool check(int i , int j , vector<vector<char>>& grid , vector<vector<bool>>&vis){
+        return ((i >= 0 && i < grid.size()) && (j >= 0 && j < grid[0].size()) && grid[i][j] == '1' && !vis[i][j]) ;
     }
-    void dfs(int i , int j , int r, int c ,vector<vector<char>>& grid ,vector<vector<bool>>&visited){
-
-        if(check(i+1 , j , r , c , grid , visited)){
-            visited[i+1][j] = true;
-            dfs(i+1 , j , r , c , grid , visited);
+    void dfs(int i , int j , vector<vector<char>>& grid , vector<vector<bool>>&vis){
+        if(check(i+1 , j , grid , vis)){
+            vis[i+1][j] = true;
+            dfs(i+1 , j , grid, vis);
         }
-        if(check(i-1 , j , r , c , grid , visited)){
-            visited[i-1][j] = true;
-            dfs(i-1 , j , r , c , grid , visited);
+        if(check(i-1 , j , grid , vis)){
+            vis[i-1][j] = true;
+            dfs(i-1 , j , grid, vis);
         }
-        if(check(i, j+1 , r , c , grid , visited)){
-            visited[i][j+1] = true;
-            dfs(i , j+1 , r , c , grid , visited);
+        if(check(i , j+1 , grid , vis)){
+            vis[i][j+1] = true;
+            dfs(i , j+1 , grid, vis);
         }
-        if(check(i , j-1 , r , c , grid , visited)){
-            visited[i][j-1] = true;
-            dfs(i , j-1 , r , c , grid , visited);
+        if(check(i , j-1 , grid , vis)){
+            vis[i][j-1] = true;
+            dfs(i , j-1 , grid, vis);
         }
     }
     int numIslands(vector<vector<char>>& grid) {
-        
-        int m = grid.size() , n = grid[0].size();
-        vector<vector<bool>>visited(m , vector<bool>(n , false));
         int count = 0 ;
+        int m = grid.size() , n = grid[0].size();
+        vector<vector<bool>>vis(m , vector<bool>(n , false));
+
         for(int i = 0 ; i<m ; i++){
             for(int j = 0 ; j<n ; j++){
-                if(grid[i][j] == '1' && !visited[i][j]){
+                if(grid[i][j] == '1' && !vis[i][j]){
                     count++;
-                    dfs(i , j , m , n , grid , visited);
+                    dfs(i , j , grid , vis);
                 }
             }
         }
+
+
         return count;
     }
 };
