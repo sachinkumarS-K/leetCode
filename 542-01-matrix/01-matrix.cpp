b060af4f793145098e0a6269b52031a174f39bae
Sachin Kumar <111720618+sachinkumarS-K@users.noreply.github.com>
2025-12-16 17:24:48 +0530

    Time: 97 ms (7.72%) | Memory: 58 MB (7.06%) - LeetSync

---
+++ 542-01-matrix/01-matrix.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int r = mat.size() , c = mat[0].size();
+
+        vector<vector<bool>>vis(r , vector<bool>(c , false));
+        vector<vector<int>>ans(r , vector<int>(c , 0));
+        queue<pair<pair<int,int> , int>>que;
+
+        for(int i = 0 ; i<r ; i++){
+            for(int j = 0 ; j<c ; j++){
+                if(mat[i][j] == 0){
+                    que.push({{i , j} , 0});
+                    vis[i][j] = true;
+                }
+            }
+        }
+        while(!que.empty()){
+            int row = que.front().first.first;
+            int col = que.front().first.second;
+            int step = que.front().second;
+            que.pop();
+
+            ans[row][col] = step;
+
+            vector<int>dr = {-1 , 0 , 1 , 0};
+            vector<int>dc = {0 , 1 , 0 , -1};
+
+            for(int i = 0 ; i<4 ; i++){
+                int nr = dr[i] + row;
+                int nc = dc[i] + col;
+                if((nr >= 0 && nr < r) && (nc >= 0 && nc < c) && !vis[nr][nc]){
+                    vis[nr][nc] = true;
+                    que.push({{nr , nc} , step+1});
+
+                }
+            }
+        }
+        return ans;
+    }
+};
