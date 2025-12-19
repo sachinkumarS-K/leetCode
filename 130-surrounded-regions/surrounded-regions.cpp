23d268f2a1b7b4580009e8198a5701cc24fdd726
Sachin Kumar <111720618+sachinkumarS-K@users.noreply.github.com>
2025-12-19 17:58:43 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (25.05%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
-    void dfs(int r , int c , vector<vector<char>>& board , vector<vector<bool>>&vis){
+    void bfs(int r , int c , vector<vector<char>>& board , vector<vector<bool>>&vis){
         int n = board.size(), m = board[0].size();
         vis[r][c] = true;
+        queue<pair<int,int>>que;
+        que.push({r,c});
 
         vector<int>dr = {-1,0,1,0};
         vector<int>dc = {0,-1,0,1};
 
-        for(int i = 0 ; i<4 ; i++){
-            int nr = r + dr[i] , nc = c + dc[i] ;
-            if(nr >= 0 && nr < n && nc >= 0 && nc < m && board[nr][nc] == 'O' && !vis[nr][nc]){
-                dfs(nr,nc , board , vis);
+        while(!que.empty()){
+            auto [row , col] = que.front();
+            que.pop();
+            for(int i = 0 ; i<4 ; i++){
+                int nRow = row + dr[i];
+                int nCol = col + dc[i];
+                if(nRow >= 0 && nRow < n && nCol >= 0 && nCol < m && board[nRow][nCol] == 'O' && !vis[nRow][nCol]){
+                    que.push({nRow , nCol});
+                    vis[nRow][nCol] = true;
+                }
             }
         }
-
     }
     void solve(vector<vector<char>>& board) {
 
@@ -24,7 +31,7 @@ public:
             for (int j = 0; j < c; j++) {
                 if ((i == 0 || j == 0 || i == r-1 || j == c-1) &&
                     board[i][j] == 'O' && !vis[i][j]) {
-                    dfs(i, j, board, vis);
+                    bfs(i, j, board, vis);
                 }
             }
         }
