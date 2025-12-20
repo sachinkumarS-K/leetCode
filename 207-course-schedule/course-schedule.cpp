2ccbb1b780ca13481216a896cbd1be8f66cf89df
Sachin Kumar <111720618+sachinkumarS-K@users.noreply.github.com>
2025-12-20 19:00:30 +0530

    Time: 6 ms (53.16%) | Memory: 20.2 MB (28.30%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,33 +1,36 @@
 class Solution {
 public:
+    bool dfs(int node , vector<vector<int>>&adj , vector<bool>&vis , vector<bool>&pathVis){
+        vis[node] = true;
+        pathVis[node] = true;
+
+        for(auto it : adj[node]){
+            if(!vis[it]){
+                if(dfs(it , adj , vis , pathVis))
+                    return true;
+            }
+            else if(pathVis[it])
+                return true;
+        }
+
+        pathVis[node] = false;
+        return false;
+    }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int>freq(numCourses , 0);
         vector<vector<int>>adj(numCourses);
 
         for(auto it : prerequisites){
-            adj[it[1]].push_back(it[0]); 
-            freq[it[0]]++;
+            adj[it[1]].push_back(it[0]);          
         }
-        int count = 0;
-        queue<int>que;
-        for(int i = 0 ; i<freq.size() ; i++){
-            if(freq[i] == 0){
-                que.push(i);
-                count++;
-            }
-        }
-        while(!que.empty()){
-            int node = que.front();
-            que.pop();
+        vector<bool>vis(numCourses , false);
+        vector<bool>pathVis(numCourses , false);
 
-            for(auto it : adj[node]){
-                freq[it]--;
-                if(freq[it] == 0){
-                    que.push(it);
-                    count++;
-                }
+        for(int i = 0 ; i<numCourses ; i++){
+            if(!vis[i]){
+                if(dfs(i , adj , vis , pathVis))
+                    return false;
             }
         }
-        return count == numCourses;
+        return true;
     }
 };
