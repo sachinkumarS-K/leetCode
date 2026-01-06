008b677a690ccc652ab00a6df247fa9a98f1f6db
Sachin Kumar <111720618+sachinkumarS-K@users.noreply.github.com>
2026-01-06 10:55:18 +0530

    Time: 3 ms (68.92%) | Memory: 107.1 MB (93.11%) - LeetSync

--- 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.cpp
+++ 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.cpp
@@ -12,29 +12,33 @@
  */
 class Solution {
 public:
+    int getLevel(TreeNode* root) {
+        if (root == NULL)
+            return 0;
+        return 1 + max(getLevel(root->left), getLevel(root->right));
+    }
+    void dfs(TreeNode *root , int i , vector<int>&arr){
+        if(!root)
+            return;
+        arr[i] += root->val;
+
+        dfs(root->left , i+1 , arr);
+        dfs(root->right , i+1 , arr);
+    }
     int maxLevelSum(TreeNode* root) {
         
-        int level = INT_MIN;
-        queue<TreeNode*> que;
-        que.push(root);
-        int j = 1, maxSum = INT_MIN;
-        while (!que.empty()) {
-            int n = que.size() , sum = 0;
-            for (int i = 0; i < n; i++) {
-                sum += que.front()->val;
-                if (que.front()->left)
-                    que.push(que.front()->left);
-                if (que.front()->right)
-                    que.push(que.front()->right);
-                que.pop();
-            }
-            if (sum > maxSum) {
-                level = j;
-                maxSum = sum;
-            }
-            j++;
-        }
+        int l =  getLevel(root);
+        vector<int> arr(l , 0);
 
-        return level;
+        dfs(root , 0 , arr);
+
+        int lev = -1 , maxSum = INT_MIN;
+        for(int i = 0 ; i<arr.size() ; i++){
+            if(maxSum < arr[i]){
+                maxSum = arr[i];
+                lev = i+1;
+            }
+        }
+        return lev;
     }
 };
