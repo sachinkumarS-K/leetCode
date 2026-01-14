caeacf0c3ff79e62ddabf910c5574a1a5e1cbd92
Sachin Kumar <111720618+sachinkumarS-K@users.noreply.github.com>
2026-01-14 11:48:21 +0530

    Time: 0 ms (100.00%) | Memory: 52.4 MB (19.82%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
-    bool solve(vector<int>&arr ,  int idx , int n , vector<int>&dp){
-        if(idx >= n - 1) 
-            return true;
-
-        if(dp[idx] != -1)
-            return dp[idx];
-
-        int k = arr[idx] ;
-        for(int i = 1 ; i<= k ; i++){
-            if(solve(arr , idx+i , n , dp))
-                return dp[idx]  = 1;
-        }
-        return dp[idx] = 0;
-    }
     bool canJump(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n, -1); 
-        return solve(nums, 0, n, dp);
+        int maxIdx = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > maxIdx)
+                return false;
+
+            maxIdx = max(maxIdx, i + nums[i]);
+
+            if (maxIdx >= n - 1)
+                return true;
+        }
+        return false;
     }
 };
