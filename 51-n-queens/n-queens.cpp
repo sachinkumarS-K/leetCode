ccf7929e885ade0d8edd83fbdbe5fb55e1b3db5e
Sachin Kumar <111720618+sachinkumarS-K@users.noreply.github.com>
2026-03-16 18:40:12 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (92.92%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
-    int queen[9];
+    int queen[10] = {-1};
     bool check(int row , int col){
         for(int i = 0 ; i<row ; i++){
             int j = queen[i];
-            if (j == col || abs(row - i) == abs(col - j))
-                return 0;
+            if(j == col || abs(row-i) == abs(col-j))
+                return false;
         }
-        return 1;
+        return true;
     }
-    void rec(int level , int n , vector<vector<string>>&ans , vector<string>&board){
-        if(level == n){
+    void solve(int r , int n , vector<vector<string>>&ans , vector<string>&board){
+        if(r == n){
             ans.push_back(board);
             return;
         }
-        for(int ch = 0 ; ch < n ; ch++){
-            if(check(level , ch)){
-                board[level][ch] = 'Q';
-                queen[level] = ch;
-                rec(level+1 , n , ans , board);
-                queen[level] = -1;
-                board[level][ch] = '.';
+        for(int i = 0 ; i<n ; i++){
+            if(check(r , i)){
+                board[r][i] = 'Q';
+                queen[r] = i;
+                solve(r+1 , n , ans , board);
+                board[r][i] = '.';
+                queen[r] = -1;
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>>ans;
-        vector<string> board(n, string(n, '.'));
-        memset(queen , -1 , sizeof(queen));
-        rec(0 , n , ans , board);
+        vector<string>board(n,string(n,'.'));
+
+        solve(0 , n , ans , board);
         return ans;
     }
 };
