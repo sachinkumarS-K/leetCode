2b01f2b92d19562098e9eb06a93f5ba1283e7580
Sachin Kumar <111720618+sachinkumarS-K@users.noreply.github.com>
2026-03-19 15:06:05 +0530

    Time: 161 ms (27.37%) | Memory: 17.5 MB (54.18%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -7,11 +7,30 @@ public:
 
         return dp[i][sum] = solve(i+1 , sum + arr[i] , total-arr[i] , arr, dp) || solve(i+1 , sum , total,  arr , dp);
     }
+    bool solveUsingTabulation(int sum , vector<int>&arr){
+        int n = arr.size();
+        vector<vector<bool>>dp(n+1 , vector<bool>(sum+1 , false));
+        for(int i = 0 ; i<= n ; i++)
+            dp[i][0] = true;
+        for(int i = n-1 ; i >= 0 ; i--){
+            for(int j = 1 ; j <= sum ; j++){
+                bool inc = false;
+                if(j-arr[i] >= 0)
+                    inc = dp[i+1][j-arr[i]];
+                bool exc = dp[i+1][j];
+
+                dp[i][j] = inc || exc ; 
+            }
+        }
+        return dp[0][sum];
+    }
     bool canPartition(vector<int>& nums) {
         int sum = 0 ;
         for(int i : nums)
             sum += i;
-        vector<vector<int>>dp(nums.size()+1 , vector<int>(sum+1 , -1));
-        return solve(0 ,0, sum , nums , dp);
+        // vector<vector<int>>dp(nums.size()+1 , vector<int>(sum+1 , -1));
+        // return solve(0 ,0, sum , nums , dp);
+        if(sum & 1) return false;
+        return solveUsingTabulation(sum/2 , nums);
     }
 };
