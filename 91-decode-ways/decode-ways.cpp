a30017dda14bd68366c54dea99f5bd9a1d15373b
Sachin Kumar <111720618+sachinkumarS-K@users.noreply.github.com>
2026-03-20 14:33:54 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (45.78%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -11,9 +11,28 @@ public:
 
         return dp[i] = ans;
     }
-    int numDecodings(string s) {
-        int n = s.size();
+    int solveUsingTabulation(string str){
+        int n = str.size();
         vector<int>dp(n+1,-1);
-        return solve(s,0 , dp);
+        dp[n] = 1;
+
+        for(int i = n-1 ; i>= 0 ; i--){
+            if(str[i] == '0'){
+                dp[i] = 0;
+                continue;
+            }
+            int ans = dp[i+1];
+            if((i+1 < str.size()) && (str[i] == '1' || str[i] == '2' && str[i+1] < '7'))
+            ans += dp[i+2];
+
+            dp[i] = ans;
+        }
+        return dp[0];
+    }
+    int numDecodings(string s) {
+        // int n = s.size();
+        // vector<int>dp(n+1,-1);
+        // return solve(s,0 , dp);
+        return solveUsingTabulation(s);
     }
 };
