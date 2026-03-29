e00c44e92afa0ee6f373bd79e9429e5c8bd55881
Sachin Kumar <111720618+sachinkumarS-K@users.noreply.github.com>
2026-03-29 11:41:39 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (6.87%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.cpp
+++ 165-compare-version-numbers/compare-version-numbers.cpp
@@ -1,26 +1,36 @@
 class Solution {
 public:
     int compareVersion(string version1, string version2) {
+        stringstream ss1(version1);
+        string token = "";
+        vector<string> word1;
+        while (getline(ss1, token, '.'))
+            word1.push_back(token);
+
+        stringstream ss2(version2);
+        vector<string> word2;
+        token = "";
+
+        while (getline(ss2, token, '.'))
+            word2.push_back(token);
+
         int i = 0 , j = 0 ;
-         int n1 = 0, n2 = 0;
-        while(i < version1.size() || j < version2.size()){
-            n1 = 0 , n2 = 0 ;
-            while(i < version1.size() && version1[i] != '.'){
-                n1 = n1*10 + (version1[i]-'0');
-                i++;
-            }
+        while(i < word1.size() || j < word2.size()){
+            int n1 = 0;
+            if(i < word1.size())
+                n1 = stoi(word1[i]);
+            int n2 = 0 ;
+            if(j < word2.size())
+                n2 = stoi(word2[i]);
+            if(n1 < n2)
+                return -1;
+            if(n1 > n2)
+                return 1;
 
-            while(j < version2.size() && version2[j] != '.'){
-                n2 = n2*10 + (version2[j]-'0');
-                j++;
-            }
-                
-            if(n1 < n2) return -1 ;
-            else if(n1 > n2)    return 1;
-
-            if(i < version1.size()) i++;
-            if(j < version2.size()) j++;          
+            i++;
+            j++;
         }
-        return 0 ;
+
+        return 0;
     }
 };
