bee7f3a3a8d29b2fe3a3cf1bee3c3c014f9211b4
Sachin Kumar <111720618+sachinkumarS-K@users.noreply.github.com>
2026-04-10 16:47:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (39.81%) - LeetSync

---
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int solve(int n, vector<int>& dp) {
+        if (n == 0 || n == 1)
+            return 1;
+        if (dp[n] != -1)
+            return dp[n];
+
+        int ans = 0;
+        for (int i = 1; i <= n; i++) {
+            int left = solve(i - 1, dp);
+            int right = solve(n - i, dp);
+            ans += left * right;
+        }
+        return dp[n] = ans;
+    }
+    int numTrees(int n) {
+        vector<int> dp(n + 1, -1);
+
+        return solve(n, dp);
+    }
+};
